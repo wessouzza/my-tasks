4f19ff4158dde2a673a8eaaee3e2399ee7fb1963
Wes Souza <99045991+wessouzza@users.noreply.github.com>
2024-11-04 17:16:25 -0300

    Update TaskBoard.jsx

--- frontend/src/components/taskBoard/TaskBoard.jsx
+++ frontend/src/components/taskBoard/TaskBoard.jsx
@@ -17,18 +17,10 @@ const TaskBoard = () => {
   const [tasks, setTasks] = useState([]);
 
   
-  const reorderTasks = async (list, start, end)=>{
+  const reorderTasks =(list, start, end)=>{
     const result = Array.from(list);
     const [removed] = result.splice(start, 1);
     result.splice(end, 0, removed);
-
-    await fetch('https://banckend-mytasks.onrender.com/api/v1/tasks/reorder', {
-      method: 'POST',
-      headers: {
-        'Content-type': 'application/json'
-      },
-      body: JSON.stringify(result)
-    });
     return result;
   }
 
